c882c4e302aeda10df789f3dd4ea7c9117cb6d66
Pierre Isabel <pierre.isabel@octo.com>
2020-05-11 16:33:33 +0200

    Fixed React event listener bug

--- microfront-with-iframe/reactapp/src/App.js
+++ microfront-with-iframe/reactapp/src/App.js
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const [msg, setMsg] = useState(getQuery());
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { msg: this.getQuery() };
+  }
 
   // Receive information via routing parameters
-  function getQuery() {
+  getQuery() {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get("input");
     if (myParam !== null && myParam.length > 0) {
@@ -17,29 +20,37 @@ function App() {
   }
 
   // Receive information via DOM events
-  window.addEventListener("message", receiveMessage.bind(this), false);
+  componentDidMount() {
+    window.addEventListener("message", this.receiveMessage, false);
+  }
 
-  function receiveMessage(event) {
+  componentWillUnmount() {
+    window.removeEventListener("message", this.receiveMessage);
+  }
+
+  receiveMessage = (event) => {
     if (event.origin !== "http://localhost:5000") {
       return;
     }
-    setMsg(event.data);
+    this.setState({ msg: event.data });
 
     // Passing information back to the parent/wrapper app
     event.source.postMessage("React: I got the message!", event.origin);
-  }
+  };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Message</p>
-        <p>
-          <code>{msg}</code>
-        </p>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Message</p>
+          <p>
+            <code>{this.state.msg}</code>
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
