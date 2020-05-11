203a613829e7b85ed621369374bfdac92cd01094
Pierre Isabel <pierre.isabel@octo.com>
2020-05-11 17:21:52 +0200

    Parent-child communication tests

--- microfront-with-iframe/cypress/integration/integration.spec.js
+++ microfront-with-iframe/cypress/integration/integration.spec.js
@@ -1,5 +1,5 @@
-describe("Integration tests", function () {
-  it("Visits the wrapper application", function () {
+describe("Home page", function () {
+  it("Home page integration", function () {
     cy.visit("http://localhost:5000");
     cy.get("#ng-container").its("0.contentDocument").should("exist");
     cy.get("#react-container").its("0.contentDocument").should("exist");
@@ -12,7 +12,7 @@ describe("Integration tests", function () {
 });
 
 describe("Communication via routing parameters", function () {
-  it("Sends the data by modifying iframes URLs", function () {
+  it("Modifies iframes URLs by submitting the input", function () {
     cy.visit("http://localhost:5000");
     cy.get("#input").type("Hello!").should("have.value", "Hello!");
     cy.get(".btn").click();
@@ -26,5 +26,35 @@ describe("Communication via routing parameters", function () {
     cy.get("#vue-container")
       .its("0.src")
       .should("eq", "http://localhost:5003/?input=Hello!");
+  });
+});
+
+describe("Communication Page", function () {
+  it("Communication page integration", function () {
+    cy.visit("http://localhost:5000/communication");
+    cy.get("#ng-app").its("0.contentDocument").should("exist");
+    cy.get("#react-app").its("0.contentDocument").should("exist");
+    cy.get("#vue-app").its("0.contentDocument").should("exist");
+    cy.get("#console");
+  });
+  it("Communicates via DOM events", function () {
+    cy.get("#input").type("Hello!").should("have.value", "Hello!");
+    cy.get(".btn").click();
+  });
+  it("Updates Micro-Frontends data", function () {
+    cy.get("#ng-app")
+      .its("0.contentDocument.body")
+      .find(".msg h3")
+      .should("contain", "Hello!");
+
+    cy.get("#react-app")
+      .its("0.contentDocument.body")
+      .find("code")
+      .should("contain", "Hello!");
+
+    cy.get("#vue-app")
+      .its("0.contentDocument.body")
+      .find(".hello h2")
+      .should("contain", "Hello!");
   });
 });
