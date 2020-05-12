7bd5dfae76698dcb3b3ba175c72c37bd3aacbaad
Pierre Isabel <pierre.isabel@octo.com>
2020-05-12 11:41:05 +0200

    Micro-Frontends callback test

--- microfront-with-iframe/cypress/integration/integration.spec.js
+++ microfront-with-iframe/cypress/integration/integration.spec.js
@@ -5,13 +5,12 @@ describe("Home page", function () {
     cy.get("#react-container").its("0.contentDocument").should("exist");
     cy.get("#vue-container").its("0.contentDocument").should("exist");
   });
+
   it("Navigates to the communication page", function () {
     cy.get("a").click();
     cy.url().should("include", "/communication");
   });
-});
 
-describe("Communication via routing parameters", function () {
   it("Modifies iframes URLs by submitting the input", function () {
     cy.visit("http://localhost:5000");
     cy.get("#input").type("Hello!").should("have.value", "Hello!");
@@ -37,10 +36,18 @@ describe("Communication Page", function () {
     cy.get("#vue-app").its("0.contentDocument").should("exist");
     cy.get("#console");
   });
+
   it("Communicates via DOM events", function () {
     cy.get("#input").type("Hello!").should("have.value", "Hello!");
     cy.get(".btn").click();
   });
+  it("Informs the user in the console element", function () {
+    cy.get("#console").should(
+      "contain",
+      "> Wrapper: Message sent to the micro-frontends"
+    );
+  });
+
   it("Updates Micro-Frontends data", function () {
     cy.get("#ng-app")
       .its("0.contentDocument.body")
@@ -56,5 +63,12 @@ describe("Communication Page", function () {
       .its("0.contentDocument.body")
       .find(".hello h2")
       .should("contain", "Hello!");
+  });
+
+  it("Updates the console with Micro-frontends callback", function () {
+    cy.get("#console")
+      .should("contain", "> Angular: I got the message!")
+      .should("contain", "> React: I got the message!")
+      .should("contain", "> Vue: I got the message!");
   });
 });
