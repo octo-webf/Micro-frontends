54adebb8d2766ac0ba10a4c7fea7de2676c8935f
Pierre Isabel <pierre.isabel@octo.com>
2020-05-13 16:07:19 +0200

    transpiled successfully (JS)

---
+++ microfront-with-js/react-3/transpiled/App.js
@@ -0,0 +1,21 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+require("./App.css");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function App() {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "thirdApp"
+  }, /*#__PURE__*/_react.default.createElement("h1", null, "React 3"), /*#__PURE__*/_react.default.createElement("p", null, "Third Micro-frontend"));
+}
+
+var _default = App;
+exports.default = _default;
