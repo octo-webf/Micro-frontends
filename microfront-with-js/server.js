4217f252d3b801e0c1c5db5ff5ee0d58bceed3c2
Pierre Isabel <pierre.isabel@octo.com>
2020-05-15 11:54:14 +0200

    Javascript composition works

--- microfront-with-js/server.js
+++ microfront-with-js/server.js
@@ -4,6 +4,7 @@ const request = require("request");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 server.set("view engine", "ejs");
+server.use(express.static(__dirname + "/public"));
 
 const createProxy = (path, target) =>
   server.use(
@@ -15,13 +16,13 @@ const createProxy = (path, target) =>
     })
   );
 
-createProxy("/react1", "https://localhost:5001/");
-createProxy("/react2", "https://localhost:5002/");
-createProxy("/react3", "https://localhost:5003/");
+createProxy("/react1", "http://localhost:5001/");
+createProxy("/react2", "http://localhost:5002/");
+createProxy("/react3", "http://localhost:5003/");
 
 server.get("/", (req, res) => res.render("index"));
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Homepage listening on port ${port}`);
 });
