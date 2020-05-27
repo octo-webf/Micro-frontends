5b186a1930a2f411ca9db3954de35c12252754f3
Pierre Isabel <pierre.isabel@octo.com>
2020-05-27 11:11:56 +0200

    JS2: static first tests

--- microfront-with-js-2/microapp2/src/App.js
+++ microfront-with-js-2/microapp2/src/App.js
@@ -1,23 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
       </header>
     </div>
   );
