829b25918271ce2e36c7da14878e9f7e773f6a9c
Pierre Isabel <pierre.isabel@octo.com>
2020-05-27 11:29:13 +0200

    WebComponents: React web component

---
+++ microfront-with-webcomponents/react-app/src/CustomElement.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CustomElement() {
+  return (
+    <div>
+      <p>React Component</p>
+    </div>
+  );
+}
+
+export default CustomElement;
--- microfront-with-webcomponents/react-app/src/index.js
+++ microfront-with-webcomponents/react-app/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import CustomElement from "./CustomElement";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class WebComponent extends HTMLElement {
+  connectedCallback() {
+    console.log("React connected");
+    ReactDOM.render(<CustomElement />, this);
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+  disconnectedCallback() {
+    console.log("React disconnected");
+    ReactDOM.unmountComponentAtNode(this);
+  }
+}
+
+window.customElements.define("react-el", WebComponent);
+
 serviceWorker.unregister();
