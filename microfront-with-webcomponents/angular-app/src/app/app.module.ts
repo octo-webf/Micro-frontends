07fec6d9cfae03bb6fdd16a77e6b6c1f1d3db4c6
Pierre Isabel <pierre.isabel@octo.com>
2020-06-02 17:19:37 +0200

    Custom Elements: Fixing Angular composition bugs

--- microfront-with-webcomponents/angular-app/src/app/app.module.ts
+++ microfront-with-webcomponents/angular-app/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Injector } from '@angular/core';
 import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
+//import { AppComponent } from './app.component';
 import { CustomComponent } from './custom/custom.component';
 
 @NgModule({
-  declarations: [AppComponent, CustomComponent],
+  declarations: [CustomComponent],
   imports: [BrowserModule],
-  providers: [],
-  bootstrap: [],
   entryComponents: [CustomComponent],
 })
 export class AppModule {
   constructor(private injector: Injector) {}
-  ngDoBootstrap(): void {
-    const { injector } = this;
 
+  ngDoBootstrap(): void {
+    const injector = this.injector;
     const ngCustomElement = createCustomElement(CustomComponent, { injector });
 
     customElements.define('angular-element', ngCustomElement);
