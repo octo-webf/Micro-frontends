5a2ed917f05a3bfe11d8eb5375b2f9121d1e7566
Pierre Isabel <pierre.isabel@octo.com>
2020-06-05 11:46:16 +0200

    Custom Elements: angular 1 as products list

--- microfront-with-webcomponents/angular-app/src/app/custom/custom.component.ts
+++ microfront-with-webcomponents/angular-app/src/app/custom/custom.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-custom',
   templateUrl: './custom.component.html',
-  styleUrls: ['./custom.component.scss']
+  styleUrls: ['./custom.component.scss'],
 })
 export class CustomComponent implements OnInit {
+  CONTENT_HOST: String = 'http://localhost:4100';
+  productList = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    fetch(`${this.CONTENT_HOST}/productList.json`)
+      .then((res) => res.json())
+      .then((list) => (this.productList = list.products))
+      .then(() => console.log(this.productList));
   }
 
+  handleClick(product) {
+    console.log(product);
+  }
 }
