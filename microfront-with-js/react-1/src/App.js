db9e2a98b8c077b3a04484f3356cc79614231a37
Pierre Isabel <pierre.isabel@octo.com>
2020-06-08 16:14:53 +0200

    JS: fixing static files loading

--- microfront-with-js/react-1/src/App.js
+++ microfront-with-js/react-1/src/App.js
@@ -2,18 +2,10 @@ import React, { useState } from "react";
 import "./App.css";
 import Component from "./Component";
 
-const PARENT_APPLICATION_URL = "http://localhost:7000";
 const MESSAGE = "Hello React!";
 
 function App() {
   const [info, setInfo] = useState(0);
-
-  const sendMessage = (message, number, bool) => {
-    window.postMessage(
-      { message, targetApp: number, clear: bool },
-      PARENT_APPLICATION_URL
-    );
-  };
 
   return (
     <div className="firstApp flex">
@@ -23,10 +15,10 @@ function App() {
       </div>
       <div class="flex">
         <button onClick={() => setInfo(info + 1)}>Clic</button>
-        <button onClick={() => sendMessage(MESSAGE, 2, false)}>React 2</button>
-        <button onClick={() => sendMessage(MESSAGE, 3, false)}>React 3</button>
-        <button onClick={() => sendMessage(MESSAGE, "*", false)}>All</button>
-        <button class="clear" onClick={() => sendMessage("", "*", true)}>
+        <button onClick={() => window.sendMessage(MESSAGE, 2, false)}>React 2</button>
+        <button onClick={() => window.sendMessage(MESSAGE, 3, false)}>React 3</button>
+        <button onClick={() => window.sendMessage(MESSAGE, "*", false)}>All</button>
+        <button class="clear" onClick={() => window.sendMessage("", "*", true)}>
           Clear
         </button>
       </div>
