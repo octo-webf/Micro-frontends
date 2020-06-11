15df6b4c0f7b6261c08178a951f7ef6d877a2408
Pierre Isabel <pierre.isabel@octo.com>
2020-06-11 18:05:55 +0200

    Custom Events: Styled vue-app

--- microfront-with-webcomponents/vue-app/src/App.vue
+++ microfront-with-webcomponents/vue-app/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <img id="vue-img" alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="First Vue Micro-Frontend" />
-    <DetailedProduct msg="Product Detail" />
+    <DetailedProduct :product="product" />
   </div>
 </template>
 
@@ -17,15 +17,31 @@ export default {
     DetailedProduct,
   },
   props: [],
+  data() {
+    return {
+      product: {
+        id: 0,
+        name: "GTA V",
+        price: "49€",
+        imgURL:
+          "https://www.cdiscount.com/pdt2/7/9/8/1/700x700/son3491951945798/rw/gta-v-ps4.jpg",
+        tags: "Adult, Violence, Drugs",
+      },
+    };
+  },
 };
 </script>
 
 <style>
 #app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-family: sans-serif;
+  margin: auto;
+}
+
+#vue-img {
   padding-top: 60px;
+  height: 150px;
 }
 </style>
--- microfront-with-webcomponents/vue-app/src/components/DetailedProduct.vue
+++ microfront-with-webcomponents/vue-app/src/components/DetailedProduct.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="hello">
-    <h3>{{ msg }}</h3>
+  <div class="item">
+    <h2>Product Detail</h2>
+    <div class="flex">
+      <div>
+        <h2>{{ product.name }}</h2>
+        <h3>{{ product.price }}</h3>
+        <h5>{{ product.tags }}</h5>
+        <div class="button-cart" v-on:click="handleClick(product)">
+          <h4>Add to cart</h4>
+        </div>
+      </div>
+      <img :src="product.imgURL" id="product-img" alt="product-img" />
+    </div>
   </div>
 </template>
 
@@ -8,14 +19,56 @@
 export default {
   name: "DetailedProduct",
   props: {
-    msg: String,
+    product: Object,
+  },
+  methods: {
+    handleClick: (product) => {
+      console.log(product);
+    },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.item {
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  box-shadow: 0 0 10px lightgray;
+  width: calc(200px + 20vw);
+  padding: calc(10px + 1vw);
+  margin: auto;
+}
+
+.item .flex {
+  text-align: left;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap-reverse;
+}
+
+.flex h2,
+h3,
+h4,
+h5 {
+  margin: 0;
+}
+.item h5 {
+  font-weight: 200;
+}
+.button-cart {
+  background-color: rgb(1, 131, 1);
+  color: white;
+  text-align: center;
+  padding: 10px 10px 10px 10px;
+  margin-top: 20px;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  box-shadow: 0 0 10px lightgray;
+  cursor: pointer;
+}
+
+#product-img {
+  max-width: 50%;
 }
 </style>
