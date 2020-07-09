2dc2fcd6b63fc82c829bc6d723c0db69ec181f55
Pierre Isabel <pierre.isabel@octo.com>
2020-07-09 17:22:19 +0200

    wc-redux: allow redux chrome extension

--- microfront-with-wc-redux/redux-app/redux/store.js
+++ microfront-with-wc-redux/redux-app/redux/store.js
@@ -1,7 +1,12 @@
 import rootReducer from "./reducers";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default store;
