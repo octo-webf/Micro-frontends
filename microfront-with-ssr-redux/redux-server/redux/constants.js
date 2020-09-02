c5a98595ee507c4c8891bcac13f46352df5699d0
Pierre Isabel <pierre.isabel@octo.com>
2020-09-02 14:10:04 +0200

    ssr-redux: removing separate store management

--- microfront-with-ssr-redux/redux-server/redux/constants.js
+++
@@ -1,4 +0,0 @@
-export const SHOW_ALL = "SHOW_ALL";
-export const SHOW_VIDEO_GAMES = "SHOW_VIDEO_GAMES";
-export const SHOW_PHONES = "SHOW_PHONES";
-export const SHOW_COMPUTERS = "SHOW_COMPUTERS";
--- microfront-with-ssr-redux/redux-server/redux/main.js
+++
@@ -1,7 +0,0 @@
-import store from "./store";
-import * as actions from "./actions";
-import * as selectors from "./selectors";
-
-window.store = store;
-window.actions = actions;
-window.selectors = selectors;
